ccd53fc0860232445591efcfabf46a6818915e1b
Ziggy Veerman <ziggy.veerman@hva.nl>
2020-05-04 15:09:39 +0200

    created basic adapter from template

--- app/src/main/java/com/example/gamebacklog/adapters/GameAdapter.kt
+++ app/src/main/java/com/example/gamebacklog/adapters/GameAdapter.kt
@@ -1,4 +1,39 @@
 package com.example.gamebacklog.adapters
 
-class GameAdapter {
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.gamebacklog.R
+import com.example.gamebacklog.model.Game
+
+class GameAdapter(private val games: List<Game>):
+    RecyclerView.Adapter<GameAdapter.ViewHolder>() {
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        fun bind(game: Game){
+
+        }
+        
+    }
+
+    /**
+     * Creates and returns a ViewHolder object, inflating a standard layout called simple_list_item_1.
+     */
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): GameAdapter.ViewHolder {
+        return ViewHolder(
+            LayoutInflater.from(parent.context).inflate(R.layout.item, parent, false)
+        )
+    }
+
+    /**
+     * Returns the size of the list
+     */
+    override fun getItemCount(): Int = games.size
+
+    /**
+     * Called by RecyclerView to display the data at the specified position.
+     */
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(games[position])
+    }
 }
