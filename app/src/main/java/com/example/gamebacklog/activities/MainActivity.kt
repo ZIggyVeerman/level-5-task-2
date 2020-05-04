56362578c32316f9297a379dce64942f47fb58d6
Ziggy Veerman <ziggy.veerman@hva.nl>
2020-05-04 14:27:37 +0200

    layout files, wat is dit toch leuk werk :( RIP

--- app/src/main/java/com/example/gamebacklog/activities/MainActivity.kt
+++ app/src/main/java/com/example/gamebacklog/activities/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.gamebacklog.activities
 
+import android.content.Intent
 import android.os.Bundle
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
@@ -14,11 +15,11 @@ class MainActivity : AppCompatActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_main)
-//    setSupportActionBar(toolbar)
+    setSupportActionBar(toolbar)
 
-    fab.setOnClickListener { view ->
-      Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-        .setAction("Action", null).show()
+    fab.setOnClickListener {
+      val intent = Intent(this, AddActivity::class.java)
+      startActivity(intent)
     }
   }
 
